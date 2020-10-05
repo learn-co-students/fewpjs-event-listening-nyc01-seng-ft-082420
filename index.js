93db24f28ae9547bb6fff6563743ee2d824d1257
sdonohue0918 <sdonohue0918@gmail.com>
2020-10-05 14:56:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+
+
 function addingEventListener() {
-}
+    input = document.getElementById('input');
+    input.addEventListener('mouseover', function(event){
+        alert('you hovered over the input tag!')
+    });
+    input.setAttribute("style", "background-color:green;")
+};
+
+addingEventListener();
